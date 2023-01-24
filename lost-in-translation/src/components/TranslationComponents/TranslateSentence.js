a1a297f87800d2700a3520fcbf1c881b90d5e61c
Maxbwee <maxbowen1234@gmail.com>
2023-01-24 17:31:06 +0200

    Added translatesentnce file

---
+++ lost-in-translation/src/components/TranslationComponents/TranslateSentence.js
@@ -0,0 +1,12 @@
+const TranslateSentence = ({sign, notes = ''}) => {
+
+    return (
+        <section>
+            <h4>Translation</h4>
+            <img src={sign.image} alt={ sign.name} width="55" />
+            <p></p>
+        </section>
+    )
+}
+
+export default TranslateSentence
